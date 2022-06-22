8250505f94ff67b3f5dcf192ecf5ea76bddd64fd
Michael Park <tjp4ca@gmail.com>
2022-06-22 16:00:20 -0400

    date update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,162 @@
 var myDate = $(".time-block")
-
-document.querySelector(".time-block").textContent = new Date();
-    console.log(document.querySelector(".time-block").textContent);
+document.querySelector(".time-block").textContent = moment().format("MMMM Do, YYYY - hh:mm:ss a");
 
 
 
+var rightNow = moment().format("MMMM Do, YYYY - hh:mm:ss a");
+console.log(rightNow);
 
-// when save button is clicked
-var save = document.querySelector(".amBtn");
-save.addEventListener("click", saveEvent)
+var futureHour = moment().add(1, "hour").format("dddd, MM-D-YYYY [at] hh:mm:ss A");
+console.log(futureHour);
+
+var pastHour = moment().subtract(1, "hour").format("dddd, MM-D-YYYY [at] hh:mm:ss A");
+console.log(pastHour);
 
 
-function saveEvent(event) {
-    textValue = document.getElementById("amText").value;
+    // convert to moment object at 5:00pm
+    // example var time = moment(date, "L").set("hour", 17);
+
+
+// 9am save button
+var save9 = document.querySelector(".nineBtn");
+save9.addEventListener("click", save9Event)
+
+function save9Event(event) {
+    textValue = document.getElementById("nineTextBox").value;
     console.log(textValue);
 
-    localStorage.setItem("nineAm", textValue);
+    localStorage.setItem("nineTextValue", textValue);
 
     // console.log(event.target.id)
     console.log("clicked")
 }
+document.getElementById("nineTextBox").textContent = localStorage.getItem("nineTextValue")
 
-document.getElementById("amText").textContent = localStorage.getItem("nineAm")
+
+// 10am save button
+var save10 = document.querySelector(".tenBtn");
+save10.addEventListener("click", save10Event)
+
+function save10Event(event) {
+    textValue = document.getElementById("tenTextBox").value;
+    console.log(textValue);
+
+    localStorage.setItem("tenTextValue", textValue);
+
+    console.log("clicked")
+}
+document.getElementById("tenTextBox").textContent = localStorage.getItem("tenTextValue")
+
+
+// 11am save button
+var save11 = document.querySelector(".elevenBtn");
+save11.addEventListener("click", save11Event)
+
+function save11Event(event) {
+    textValue = document.getElementById("elevenTextBox").value;
+    console.log(textValue);
+
+    localStorage.setItem("elevenTextValue", textValue);
+
+    console.log("clicked")
+}
+document.getElementById("elevenTextBox").textContent = localStorage.getItem("elevenTextValue")
+
+
+// 12pm save button
+var save12 = document.querySelector(".twelveBtn");
+save12.addEventListener("click", save12Event)
+
+function save12Event() {
+    textValue = document.getElementById("twelveTextBox").value;
+    console.log(textValue);
+
+    localStorage.setItem("twelveTextValue", textValue);
+
+    console.log("clicked");
+}
+document.getElementById("twelveTextBox").textContent = localStorage.getItem("twelveTextValue")
+
+
+// 1pm save button
+var save1 = document.querySelector(".oneBtn");
+save1.addEventListener("click", save1Event)
+
+function save1Event() {
+    textValue = document.getElementById("oneTextBox").value;
+    console.log(textValue);
+
+    localStorage.setItem("oneTextValue", textValue);
+
+    console.log("clicked");
+}
+document.getElementById("oneTextBox").textContent = localStorage.getItem("oneTextValue")
+
+
+// 2pm save button
+var save2 = document.querySelector(".twoBtn");
+save2.addEventListener("click", save2Event)
+
+function save2Event() {
+    textValue = document.getElementById("twoTextBox").value;
+    console.log(textValue);
+
+    localStorage.setItem("twoTextValue", textValue);
+
+    console.log("clicked");
+}
+document.getElementById("twoTextBox").textContent = localStorage.getItem("twoTextValue")
+
+
+// 3pm save button
+var save3 = document.querySelector(".threeBtn");
+save3.addEventListener("click", save3Event)
+
+function save3Event() {
+    textValue = document.getElementById("threeTextBox").value;
+    console.log(textValue);
+
+    localStorage.setItem("threeTextValue", textValue);
+
+    console.log("clicked");
+}
+document.getElementById("threeTextBox").textContent = localStorage.getItem("threeTextValue")
+
+
+// 4pm save button
+var save4 = document.querySelector(".fourBtn");
+save4.addEventListener("click", save4Event)
+
+function save4Event() {
+    textValue = document.getElementById("fourTextBox").value;
+    console.log(textValue);
+
+    localStorage.setItem("fourTextValue", textValue);
+
+    console.log("clicked");
+}
+document.getElementById("fourTextBox").textContent = localStorage.getItem("fourTextValue")
+
+
+// 5pm save button
+var save5 = document.querySelector(".fiveBtn");
+save5.addEventListener("click", save5Event)
+
+function save5Event() {
+    textValue = document.getElementById("fiveTextBox").value;
+    console.log(textValue);
+
+    localStorage.setItem("fiveTextValue", textValue);
+
+    console.log("clicked");
+}
+document.getElementById("fiveTextBox").textContent = localStorage.getItem("fiveTextValue")
+
+
+
+
+
+
 
 
 
